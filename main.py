54ecac177fd53c87f479a0118a4cf7a6b82044e0
Rafayel Latif <Rafayel.latif@gmail.com>
2024-10-10 17:35:08 +0100

    Added embedding bucketing model to give genres to the titles

--- main.py
+++ main.py
@@ -7,13 +7,24 @@ import requests
 from bs4 import BeautifulSoup
 
 import embedding_bucketing.embedding_model_test as em
+from config import openai_api_key
+
+
+
 
 # Initialize global variables
 if "videos_in_list" not in st.session_state:
     st.session_state.videos_in_list = []
 display_video = False
 
+max_distance = 20 # setting it high for no auto bucketing
+amount_of_binary_digits = 10
+type_of_distance_calc = "COSINE SIMILARITY"
+start_Genre = ["Drama", "Commedy", "Action", "romance", "documentry"]
+em.config(openai_api_key) # configuring openai client for embedding model
 cache_file_name = "genre_embedding_cache.json"
+cache, genre_buckets = em.init(cache_file_name, start_Genre)
+
 
 st.set_page_config(page_title="DemoRS", layout="wide")
 
@@ -52,6 +63,16 @@ def get_title_from_url(url):
     print("title", title)
     return title
 
+def embedding_bucketing_response(uncategorized_input, max_distance, bucket_list, type_of_distance_calc, amount_of_binary_digits):
+    sort_response = em.auto_sort(uncategorized_input, max_distance, bucket_list, type_of_distance_calc, amount_of_binary_digits) 
+
+    closest_distance = sort_response[0]
+    closest_bucket   = sort_response[1]  # which bucket the uncategorized_input was placed in
+    bucket_id        = sort_response[2]  # the id of the closest_bucket
+    bucket_binary    = sort_response[3]  # binary representation of the id for INPUT into api.aolabs.ai
+
+    return closest_bucket, bucket_binary # returning the closest bucket and its binary encoding
+
 
 # Title of the app
 st.title("Recommender")
@@ -77,6 +98,9 @@ if st.button("Next"):
         st.session_state.videos_in_list.pop(0)  # Remove the first video from the list
         if st.session_state.videos_in_list:
             title = get_title_from_url(st.session_state.videos_in_list[0])
+            closest_genre, genre_binary_encoding = embedding_bucketing_response(title, max_distance, genre_buckets, type_of_distance_calc, amount_of_binary_digits)
+            print("Closest genre to title", title, "is", closest_genre)
+            st.write("Genre:", closest_genre)
             st.video(st.session_state.videos_in_list[0])
         else:
             st.write("No more videos in the list.")
