41ccb12b3f83894f3887a093596dfa260da5dbf5
Rafayel Latif <Rafayel.latif@gmail.com>
2024-10-10 18:43:23 +0100

    Fixed floating reult and video, getting ready to connect agent

--- arch_recommender.py
+++ arch_recommender.py
@@ -1,2 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+// aolabs.ai software >ao_core/Arch.py (C) 2023 Animo Omnis Corporation. All Rights Reserved.
+
+Thank you for your curiosity!
+
+Arch file for recommender
+"""
+
 import ao_arch as ar
+
+description = "Basic Recommender System"
+
+#genre, length,  Fnf
+arch_i = [10]     
+arch_z = [1]           
+arch_c = []           
+connector_function = "full_conn"
+
+# To maintain compatability with our API, do not change the variable name "Arch" or the constructor class "ao.Arch" in the line below (the API is pre-loaded with a version of the Arch class in this repo's main branch, hence "ao.Arch")
+arch = ar.Arch(arch_i, arch_z, arch_c, connector_function, description)
 
--- main.py
+++ main.py
@@ -9,18 +9,44 @@ from bs4 import BeautifulSoup
 import embedding_bucketing.embedding_model_test as em
 from config import openai_api_key
 
-
-
+import ao_core as ao
+from arch_recommender import arch
 
 # Initialize global variables
 if "videos_in_list" not in st.session_state:
     st.session_state.videos_in_list = []
+
 display_video = False
+
+if "agent" not in st.session_state:
+    print("-------creating agent-------")
+    st.session_state.agent = ao.Agent(arch, notes=[])
 
 max_distance = 20 # setting it high for no auto bucketing
 amount_of_binary_digits = 10
 type_of_distance_calc = "COSINE SIMILARITY"
-start_Genre = ["Drama", "Commedy", "Action", "romance", "documentry"]
+start_Genre = genres = [
+    "Drama", 
+    "Comedy", 
+    "Action", 
+    "Romance", 
+    "Documentary", 
+    "Music", 
+    "Gaming", 
+    "Entertainment", 
+    "News", 
+    "Thriller", 
+    "Horror", 
+    "Science Fiction", 
+    "Fantasy", 
+    "Adventure", 
+    "Mystery", 
+    "Animation", 
+    "Family", 
+    "Historical", 
+    "Biography", 
+    "Superhero"
+]
 em.config(openai_api_key) # configuring openai client for embedding model
 cache_file_name = "genre_embedding_cache.json"
 cache, genre_buckets = em.init(cache_file_name, start_Genre)
@@ -29,7 +55,18 @@ cache, genre_buckets = em.init(cache_file_name, start_Genre)
 st.set_page_config(page_title="DemoRS", layout="wide")
 
 # Predefined list of random search terms
-random_search_terms = ['funny', 'music', 'gaming', 'science', 'technology', 'news', 'random', 'adventure', "programming", "computer science"]
+random_search_terms = ['funny', 'gaming', 'science', 'technology', 'news', 'random', 'adventure', "programming", "computer science"]
+
+def train_agent(input, user_response):  # function to train the agent
+    if user_response == "pleasure":
+        Cpos = True 
+        Cneg = False
+    elif user_response == "pain":
+        Cneg = True
+        Cpos = False
+    
+    st.session_state.agent.next_state(INPUT=input, Cpos=Cpos, Cneg=Cneg, print_result=False)
+
 
 def get_random_youtube_link():
     print("Attempting to get a random video link...")
@@ -74,6 +111,15 @@ def embedding_bucketing_response(uncategorized_input, max_distance, bucket_list,
     return closest_bucket, bucket_binary # returning the closest bucket and its binary encoding
 
 
+def next_video():
+    display_video = False
+    title = get_title_from_url(st.session_state.videos_in_list[0])
+    closest_genre, genre_binary_encoding = embedding_bucketing_response(title, max_distance, genre_buckets, type_of_distance_calc, amount_of_binary_digits)
+    print("Closest genre to title", title, "is", closest_genre)
+    st.write("Genre:", closest_genre)
+    st.video(st.session_state.videos_in_list[0])
+    return closest_genre, genre_binary_encoding
+
 # Title of the app
 st.title("Recommender")
 
@@ -91,18 +137,28 @@ if st.button("Start"):
                 data = get_random_youtube_link()
             st.session_state.videos_in_list.append(data)
         st.write(f"Loaded {count} videos.")
+        genre, genre_binary_encoding = next_video()
+small_right, small_left = st.columns(2)
 
-# Next button logic
-if st.button("Next"):
-    if len(st.session_state.videos_in_list) > 0:
-        st.session_state.videos_in_list.pop(0)  # Remove the first video from the list
-        if st.session_state.videos_in_list:
-            title = get_title_from_url(st.session_state.videos_in_list[0])
-            closest_genre, genre_binary_encoding = embedding_bucketing_response(title, max_distance, genre_buckets, type_of_distance_calc, amount_of_binary_digits)
-            print("Closest genre to title", title, "is", closest_genre)
-            st.write("Genre:", closest_genre)
-            st.video(st.session_state.videos_in_list[0])
+with small_right:
+    if st.button("Pleasure"):
+        if len(st.session_state.videos_in_list) > 0:
+            st.session_state.videos_in_list.pop(0)  # Remove the first video from the list
+            display_video = True
         else:
-            st.write("No more videos in the list.")
-    else:
-        st.write("The list is empty, cannot pop any more items.")
+            st.write("The list is empty, cannot pop any more items.")
+
+with small_left:
+    if st.button("Pain"):
+        if len(st.session_state.videos_in_list) > 0:
+            st.session_state.videos_in_list.pop(0)  # Remove the first video from the list
+            display_video = True
+        else:
+            st.write("The list is empty, cannot pop any more items.")
+
+
+if st.session_state.videos_in_list:
+    if display_video == True:
+        genre, genre_binary_encoding = next_video()
+else:
+    st.write("No more videos in the list.")
